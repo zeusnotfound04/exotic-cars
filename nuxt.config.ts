2cb5e7fa7164101ead1748f7383393cb02ad13ff
Jose Tiznado <josetiznado@gmail.com>
2024-02-24 19:39:31 -0600

    - Added pwa files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   app: {
     // pageTransition: { name: 'page', mode: 'out-in' },
@@ -8,11 +11,14 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+        { name:"theme-color", content:"#ffffff"}
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'stylesheet', type: "text/css", href: 'https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap' },
+        { rel:"manifest", href:"site.webmanifest"},
+        { rel:"mask-icon", href:"safari-pinned-tab.svg", color:"#5bbad5"}
       ],  
       script: [
         {src:"js/jquery.min.js"},
@@ -36,7 +42,6 @@ export default defineNuxtConfig({
 
   modules: [
     // ...
-    // '@nuxt/ui',
     [
       '@pinia/nuxt',
       {
@@ -46,16 +51,38 @@ export default defineNuxtConfig({
         ],
       },
     ],
-    '@nuxt/ui',
-    // '@nuxtjs/date-fns',
+
     // '@nuxtjs/pwa',
-    '@vite-pwa/nuxt',
-    // '@nuxtjs/axios'
     
   ],
-  pwa: {
-    /* your pwa options */
-  },
+// PWA module configuration: https://go.nuxtjs.dev/pwa
+// pwa: {
+//   // https://pwa.nuxtjs.org/manifest
+//   // Manifest adds Web App Manifest with no pain.
+//   manifest: {
+//     name: "SportsWire",
+//     short_name: "SportsWire",
+//     description: "Up-to-date Sports News and Analysis App.",
+//     theme_color: "#6a5acd",
+//     lang: "en",
+//     background_color: "#6a5acd",
+//   },
+//   // https://pwa.nuxtjs.org/icon
+//   icon: {
+//     sizes: [64, 120, 144, 152, 192, 384, 512], //Array of sizes to be generated (Square).These are the default values
+//   },
+//   // https://pwa.nuxtjs.org/meta
+//   // Meta easily adds common meta tags into your project with zero-config needed. 
+//   meta: {
+//     name: "SportsWire",
+//     description: "Up-to-date Sports News and Analysis App.",
+//     author: "Vino Samari",
+//     theme_color: "#6a5acd",
+//     nativeUi: true,
+//     appleStatusBarStyle: "black",
+//     mobileAppIOS: true,
+//   },
+// },
   // axios: {
   //   // proxy: true
   // },
@@ -77,5 +104,6 @@ export default defineNuxtConfig({
     '/api/**': {
         proxy: { to: "http://localhost:3002/api/**", },
     }
-  }
+  },
+
 });
